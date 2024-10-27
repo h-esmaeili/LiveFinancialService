2a6ce230b90eaaf5e2548b3e32a0bef693c2ba71
hamid <hamid@abacus.co>
2024-10-27 22:42:40 +0330

    Add InstrumentsController with endpoints for retrieving instrument list and current price

--- Server/MarketPulse.Api/Program.cs
+++ Server/MarketPulse.Api/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,5 +15,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
